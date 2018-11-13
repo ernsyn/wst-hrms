2a52b4dfcc58c7d0e1f659b4b99ef5ecd7300eb3
SMSB\nsch <nsch@syntronic.com>
2018-11-13 14:33:37 +0800

    NEW: ADD MIGRATION UPDATE EMPLOYEE_JOBS TABLE

---
+++ database/migrations/2018_11_13_142656_update_job_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateJobTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employee_jobs', function (Blueprint $table) {
+            $table->date('end_date')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
