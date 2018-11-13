cba53a7f464b74fb287481bf1df63a6e562cab78
chennee <chennee.lim@wisetech.com.my>
2018-11-13 11:10:50 +0800

    FIX: data migration for payroll

--- database/migrations/2018_11_09_050100_create_payroll_related_table.php
+++ database/migrations/2018_11_09_050100_create_payroll_related_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class CreatePayrollRelatedTable extends Migration {
 
@@ -14,8 +15,8 @@ class CreatePayrollRelatedTable extends Migration {
 	{
 	    Schema::create('addition_master', function(Blueprint $table)
 	    {
-	        $table->bigInteger('id', true);
-	        $table->bigInteger('company_id');
+	        $table->bigIncrements('id', true);
+	        $table->integer('company_id');
 	        $table->string('code')->unique('code');
 	        $table->string('name');
 	        $table->string('type');
@@ -24,22 +25,23 @@ class CreatePayrollRelatedTable extends Migration {
 	        $table->string('statutory')->nullable();
 	        $table->bigInteger('ea_form_id');
 	        $table->integer('confirmed_employee');
-	        $table->integer('status');
-	        $table->bigInteger('created_by');
+	        $table->integer('status')->default(1);
+	        $table->integer('created_by');
+	        $table->integer('updated_by')->nullable();
 	        $table->timestamps();
 	    });
 	    
 		Schema::create('addition_applies_to', function(Blueprint $table)
 		{
-			$table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 			$table->bigInteger('addition_master_id');
 			$table->bigInteger('job_master_id');
 		});
 		
 		Schema::create('deduction_master', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
-		    $table->bigInteger('company_id');
+		    $table->bigIncrements('id', true);
+		    $table->integer('company_id');
 		    $table->string('code')->unique('code');
 		    $table->string('name');
 		    $table->string('type');
@@ -47,71 +49,80 @@ class CreatePayrollRelatedTable extends Migration {
 		    $table->decimal('amount', 9);
 		    $table->string('statutory')->nullable();
 		    $table->integer('confirmed_employee');
-		    $table->integer('status');
-		    $table->bigInteger('created_by');
+		    $table->integer('status')->default(1);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 		Schema::create('deduction_applies_to', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 		    $table->bigInteger('deduction_master_id');
 		    $table->bigInteger('job_master_id');
 		});
 		
 		Schema::create('eis', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 		    $table->decimal('salary', 9);
 		    $table->decimal('employer', 9);
 		    $table->decimal('employee', 9);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 		Schema::create('epf', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 		    $table->string('category');
 		    $table->decimal('salary', 9);
 		    $table->decimal('employer', 9);
 		    $table->decimal('employee', 9);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 		Schema::create('pcb', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 		    $table->decimal('salary', 9);
 		    $table->integer('category');
-		    $table->integer('number_of_children', 9);
+		    $table->integer('number_of_children');
 		    $table->decimal('amount', 9);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 		Schema::create('socso', function(Blueprint $table)
 		{
-		    $table->bigInteger('id', true);
+		    $table->bigIncrements('id', true);
 		    $table->decimal('salary', 9);
 		    $table->decimal('first_category_employer', 9);
 		    $table->decimal('first_category_employee', 9);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 		Schema::create('payroll_master', function (Blueprint $table) {
 		    $table->bigIncrements('id');
-		    $table->bigInteger('company_id');
+		    $table->integer('company_id');
 		    $table->date('year_month');
 		    $table->smallInteger('period');
-		    $table->smallInteger('status');
-		    $table->bigInteger('created_by');
-		    $table->bigInteger('updated_by');
+		    $table->smallInteger('status')->default(1);
+		    $table->integer('created_by');
+		    $table->integer('updated_by')->nullable();
 		    $table->timestamps();
 		});
 		
 	    Schema::create('payroll_trx', function (Blueprint $table) {
 	        $table->bigIncrements('id');
 	        $table->unsignedBigInteger('payroll_master_id');
-	        $table->bigInteger('employee_id');
+	        $table->integer('employee_id');
 	        $table->decimal('employee_epf', 9);
 	        $table->decimal('employee_eis', 9);
 	        $table->decimal('employee_socso', 9);
@@ -119,14 +130,14 @@ class CreatePayrollRelatedTable extends Migration {
 	        $table->decimal('employer_epf', 9);
 	        $table->decimal('employer_eis', 9);
 	        $table->decimal('employer_socso', 9);
-	        $table->decimal('kpi', 9);
-	        $table->decimal('bonus', 9);
-	        $table->decimal('seniority_pay', 9);
-	        $table->decimal('basic_salary', 9);
-	        $table->decimal('take_home_pay', 9);
+	        $table->decimal('kpi', 9)->default(0);
+	        $table->decimal('bonus', 9)->default(0);
+	        $table->decimal('seniority_pay', 9)->default(0);
+	        $table->decimal('basic_salary', 9)->default(0);
+	        $table->decimal('take_home_pay', 9)->default(0);
 	        $table->longText('note')->nullable();
-	        $table->bigInteger('created_by');
-	        $table->integer('updated_by');
+	        $table->integer('created_by');
+	        $table->integer('updated_by')->nullable();
 	        $table->timestamps();
 	    });
 	    
@@ -136,8 +147,8 @@ class CreatePayrollRelatedTable extends Migration {
             $table->bigInteger('addition_master_id');
             $table->decimal('amount', 9);
             $table->integer('days');
-            $table->bigInteger('created_by');
-            $table->integer('updated_by');
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
             $table->timestamps();
         });
         
@@ -147,8 +158,8 @@ class CreatePayrollRelatedTable extends Migration {
             $table->bigInteger('deduction_master_id');
             $table->decimal('amount', 9);
             $table->integer('days');
-            $table->bigInteger('created_by');
-            $table->integer('updated_by');
+            $table->integer('created_by');
+            $table->integer('updated_by')->nullable();
             $table->timestamps();
         });
 	}
