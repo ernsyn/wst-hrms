91fe8afb1577c740363febb7bce1b9dbc63f5d7b
Adrian Miller <asmi@syntronic.com>
2018-11-19 18:20:49 +0800

    NEW: Super Admin Dashboard -  Added initialization check for super-admin user - prompt ENH: Remove gradient on button and reset colors to bootstrap defaults

--- app/Http/Controllers/SuperAdmin/DashboardController.php
+++ app/Http/Controllers/SuperAdmin/DashboardController.php
@@ -5,6 +5,14 @@ namespace App\Http\Controllers\SuperAdmin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Company;
+use App\CostCentre;
+use App\Department;
+use App\Branch;
+use App\Team;
+use App\EmployeePosition;
+use App\EmployeeGrade;
+
 class DashboardController extends Controller
 {
     public function __construct()
@@ -20,6 +28,24 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('pages.super-admin.dashboard');
+        $initCheck = array(
+            "companiesCount" => Company::count(),
+            "costCentresCount" => CostCentre::count(),
+            "departmentsCount" => Department::count(),
+            "branchesCount" => Branch::count(),
+            "teamsCount" => Team::count(),
+            "employeePositionsCount" => EmployeePosition::count(),
+            "employeeGradesCount" => EmployeeGrade::count(),
+        );
+
+        $initOk = true;
+        foreach ($initCheck as $count) {
+            if($count <= 0) {
+                $initOk = false;
+            }
+        }
+        $initCheck["ok"] = $initOk;
+
+        return view('pages.super-admin.dashboard', ['initCheck' => $initCheck]);
     }
 }
--- resources/views/pages/super-admin/dashboard.blade.php
+++ resources/views/pages/super-admin/dashboard.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.super-admin-base') 
 @section('content')
-<div>
-    Super Admin Dashboard
+<div id="page-super-admin-dashboard" class="container-fluid">
+    {{-- {!! dd($initCheck) !!} --}}
+    @if(!$initCheck["ok"])
+    <div id="init-warning-prompt" class="card bg-danger">
+        <div class="card-header text-white">
+            Application has not been fully initialized
+        </div>  
+        <ul class="list-group list-group-flush">
+            @if($initCheck["companiesCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Company</b> created</span><a href="{{ route('admin.settings.setting-company') }}"  class="btn btn-sm btn-primary float-right">Add Company</a></li>
+            @endif
+            @if($initCheck["costCentresCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Cost Centre</b> created</span><a href="{{ route('admin.settings.setting-cost-centre') }}"  class="btn btn-sm btn-primary float-right">Add Cost Centre</a></li>
+            @endif
+            @if($initCheck["departmentsCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Department</b> created</span><a href="{{ route('admin.settings.setting-department') }}"  class="btn btn-sm btn-primary float-right">Add Department</a></li>
+            @endif
+            @if($initCheck["branchesCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Branch</b> created</span><a href="{{ route('admin.settings.setting-branch') }}"  class="btn btn-sm btn-primary float-right">Add Branch</a></li>
+            @endif
+            @if($initCheck["teamsCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Team</b> created</span><a href="{{ route('admin.settings.setting-team') }}"  class="btn btn-sm btn-primary float-right">Add Team</a></li>
+            @endif
+            @if($initCheck["employeePositionsCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Employee Position</b> created</span><a href="{{ route('admin.settings.setting-position') }}"  class="btn btn-sm btn-primary float-right">Add Position</a></li>
+            @endif
+            @if($initCheck["employeeGradesCount"] <= 0)
+                <li class="init-warning list-group-item"><span>There should be at least one <b>Employee Grade</b> created</span><a href="{{ route('admin.settings.setting-grade') }}"  class="btn btn-sm btn-primary float-right">Add Grade</a></li>
+            @endif
+        </ul>
+    </div>
+    @endif
 </div>
 @endsection
