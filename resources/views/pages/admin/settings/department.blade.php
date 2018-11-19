a73b8259c5a42364f67073783fa5289ad8416b91
Adrian Miller <asmi@syntronic.com>
2018-11-20 06:23:43 +0800

    NEW: Redesign (Table) - Departments FIX: Add Department page

--- resources/views/pages/admin/settings/department.blade.php
+++ resources/views/pages/admin/settings/department.blade.php
@@ -1,43 +1,82 @@
 @extends('layouts.admin-base') 
 @section('content')
 
-<div class="p-4">
-        <div class="card p-4">
-            <div class="card-body">
-                <div class="row pb-3">
-                    <div class="col-auto mr-auto"></div>
-                    <div class="col-auto">
-                            <a role="button" class="btn btn-primary" href="{{ route('admin.settings.departments.add') }}">
+<div class="container">
+
+    <div class="row pb-3">
+        <div class="col-auto mr-auto"></div>
+        <div class="col-auto">
+            <a role="button" class="btn btn-primary" href="{{ route('admin.settings.departments.add') }}">
                                     Add Department
                                 </a>
-                            </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="float-right tableTools-container"></div>
-                        <table class="table display compact table-striped table-bordered table-hover w-100" id="setupJobconfigureDeptTable">
-                            <thead>
-                                <tr>
-                                        <th>No</th>
-                                        <th>Name</th>
-                                        <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                    @foreach($departments as $department)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{$department['name']}}</td>
-                                        <td>       <a class="btn btn-primary" href="{{ route('admin.settings.departments.edit', ['id' => $department->id]) }}" role="button">Edit</a>
-                                           
-                                        </td>
-                                    </tr>
-                                    @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
-    @endsection
+    <div class="row">
+        <div class="col-md-12">
+            <div class="float-right tableTools-container"></div>
+            <table class="hrms-data-table compact w-100 t-2" id="departments-table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($departments as $department)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{$department['name']}}</td>
+                        <td>
+                            <button onclick="window.location='{{ route('admin.settings.departments.edit', ['id' => $department->id]) }}';"
+                                    class="round-btn btn btn-default fas fa-edit btn-segment">
+                                </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
+    </div>
+</div>
+@endsection
+ 
+@section('scripts')
+<script>
+    $('#departments-table').DataTable({
+            responsive: true,
+            stateSave: true,
+            dom: `<'row d-flex'<'col'l><'col d-flex justify-content-end'f><'col-auto d-flex justify-content-end'B>>" +
+            <'row'<'col-md-6'><'col-md-6'>>
+            <'row'<'col-md-12't>><'row'<'col-md-12'ip>>`,
+            buttons: [{
+                    extend: 'copy',
+                    text: '<i class="fas fa-copy "></i>',
+                    // text: '<i class="fas fa-copy "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Copy'
+                },
+                {
+                    extend: 'colvis',
+                    text: '<i class="fas fa-search "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Show/Hide Column'
+                },
+                {
+                    extend: 'csv',
+                    text: '<i class="fas fa-file-alt "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Export CSV'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fas fa-print "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Print'
+                },
+            ]
+    
+        });
+</script>
+@append
