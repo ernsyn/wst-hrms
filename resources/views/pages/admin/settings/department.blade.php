ee59079853cde8c818decf28ce6719b0400992cb
SMSB\nsch <nsch@syntronic.com>
2018-11-19 22:09:05 +0800

    NEW: add admin blade folder and add setting page for each functions .

---
+++ resources/views/pages/admin/settings/department.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.base') 
+@section('content')
+
+<!-- ADD -->
+<div class="modal fade" id="addDepartmentPopup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Department</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('add_department') }}" id="add_department">
+                    @csrf
+                    <div class="row p-3">
+                        <div class="form-group row w-100">
+                            <label class="col-md-12 col-form-label">Name*</label>
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="Name here"
+                                    name="name" value="{{ old('name') }}" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">
+                        {{ __('Submit') }}
+                        </button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- UPDATE -->
+<div class="modal fade" id="updateDepartmentPopup" tabindex="-1" role="dialog" aria-labelledby="updateDepartmentLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="updateDepartmentLabel">Edit Department</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('edit_department') }}" id="edit_visa">
+                    @csrf
+                    <div class="row p-3">
+                        <div class="form-group row w-100">
+                            <input id="department_id" name="department_id" type="text" hidden>
+                            <label class="col-md-12 col-form-label">Name*</label>
+                            <div class="col-md-12">
+                                <input class="form-control" id="department_name" name="department_name" type="text">
+                            </div>  
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Submit') }}
+                            </button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>    
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="p-4">
+        <div class="card p-4">
+            <div class="card-body">
+                <div class="row pb-3">
+                    <div class="col-auto mr-auto"></div>
+                    <div class="col-auto">
+                        <button type="button" class="btn btn-outline-primary waves-effect" data-toggle="modal" data-target="#addDepartmentPopup">
+                                Add Department
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="float-right tableTools-container"></div>
+                        <table class="table display compact table-striped table-bordered table-hover w-100" id="setupJobconfigureDeptTable">
+                            <thead>
+                                <tr>
+                                        <th>No</th>
+                                        <th>Name</th>
+                                        <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach($departments as $row)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{$row['name']}}</td>
+                                        <td><button class="btn btn-outline-primary waves-effect" data-toggle="modal"
+                                            data-department-id="{{$row['id']}}"
+                                            data-department-name="{{$row['name']}}"
+                                            data-target="#updateDepartmentPopup">EDIT</button></td>
+                                    </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
---
+++ resources/views/pages/admin/settings/job.blade.php
@@ -0,0 +1,150 @@
+@extends('layouts.app') 
+@section('pageTitle', 'Job Configure') 
+@section('content')
+<div class="p-4">
+    <div class="card py-4">
+        <div class="card-body">
+            <div class="container-fluid">
+                <form>
+                    <div class="row">
+                        <nav class="col-sm-12">
+                            <div class="nav nav-tabs font-weight-bold" id="nav-tab" role="tablist">
+                                <a class="nav-item nav-link active" id="nav-cost-tab" data-toggle="tab" href="#nav-cost" role="tab" aria-controls="nav-cost"
+                                    aria-selected="false">Cost Centre</a>
+                                <a class="nav-item nav-link" id="nav-department-tab" data-toggle="tab" href="#nav-department" role="tab" aria-controls="nav-department"
+                                    aria-selected="true">Department</a>
+                                <a class="nav-item nav-link" id="nav-team-tab" data-toggle="tab" href="#nav-team" role="tab" aria-controls="nav-team" aria-selected="true">Team</a>
+                                <a class="nav-item nav-link" id="nav-position-tab" data-toggle="tab" href="#nav-position" role="tab" aria-controls="nav-position"
+                                    aria-selected="true">Position</a>
+                                <a class="nav-item nav-link" id="nav-grade-tab" data-toggle="tab" href="#nav-grade" role="tab" aria-controls="nav-grade"
+                                    aria-selected="true">Grade</a>
+
+                            </div>
+                        </nav>
+                        {{-- Cost Centre --}}
+                        <div class="tab-content col-sm-12 text-justify pt-4" id="nav-tabContent">
+                            <div class="tab-pane fade show active" id="nav-cost" role="tabpanel" aria-labelledby="nav-cost-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Seniority Pay</th>
+                                                <th>Amount</th>
+                                                <th>Payroll Type</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            @foreach($costs as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{$row['name']}}</td>
+                                                <td>{{$row['seniority_pay']}}</td>
+                                                <td>{{$row['amount']}}</td>
+                                                <td>{{$row['payroll_type']}}</td>
+                                                <td>Action</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- Department --}}
+                            <div class="tab-pane fade" id="nav-department" role="tabpanel" aria-labelledby="nav-department-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            @foreach($departments as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{$row['name']}}</td>
+                                                <td>Action</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Team --}}
+                            <div class="tab-pane fade" id="nav-team" role="tabpanel" aria-labelledby="nav-team-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            @foreach($teams as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{$row['name']}}</td>
+                                                <td>Action</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Position --}}
+                            <div class="tab-pane fade" id="nav-position" role="tabpanel" aria-labelledby="nav-position-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            @foreach($positions as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{$row['name']}}</td>
+                                                <td>Action</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+
+                            {{-- Grade --}}
+                            <div class="tab-pane fade" id="nav-grade" role="tabpanel" aria-labelledby="nav-grade-tab">
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-hover">
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Name</th>
+                                                <th>Action</th>
+                                            </tr>
+                                            @foreach($grade as $row)
+                                            <tr>
+                                                <td>{{ $loop->iteration }}</td>
+                                                <td>{{$row['name']}}</td>
+                                                <td>Action</td>
+                                            </tr>
+                                            @endforeach
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            {{--
+                            <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                                This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.
+                            </div> --}}
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
