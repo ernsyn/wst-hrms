8cfff532ea89ca64ccb28594822885417651a3dc
Adrian Miller <asmi@syntronic.com>
2018-11-20 06:16:15 +0800

    NEW: Redesign (Table) - Grade FIX: Add Grade page

--- resources/views/pages/admin/settings/grade.blade.php
+++ resources/views/pages/admin/settings/grade.blade.php
@@ -2,45 +2,83 @@
 @section('content')
 
 
-<div class="p-4">
-        <div class="card p-4">
-            <div class="card-body">
-                <div class="row pb-3">
-                    <div class="col-auto mr-auto"></div>
-                    <div class="col-auto">
-                        <a role="button" class="btn btn-primary" href="{{ route('admin.settings.grades.add') }}">
-                                Add Grade
-                            </a>
-                        </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
-                        <div class="float-right tableTools-container"></div>
-                        <table class="table display compact table-striped table-bordered table-hover w-100" id="setupJobconfigureCostCentreTable">
-                            <thead>
-                                    <tr>
-                                            <th>No</th>
-                                            <th>Name</th>
-                                            <th>Action</th>
-                                        </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($grades as $grade)
-                                <tr>
-                                    <td>{{ $loop->iteration }}</td>
-                                    <td>{{$grade['name']}}</td>
-                              
-<td>       <a class="btn btn-primary" href="{{ route('admin.settings.grades.edit.post', ['id' => $grade->id]) }}" role="button">Edit</a>
-                                       
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+<div class="container">
+
+    <div class="row pb-3">
+        <div class="col-auto mr-auto"></div>
+        <div class="col-auto">
+            <a role="button" class="btn btn-primary" href="{{ route('admin.settings.grades.add') }}">
+                Add Grade
+            </a>
         </div>
     </div>
-    @endsection
+    <div class="row">
+        <div class="col-md-12">
+            <div class="float-right tableTools-container"></div>
+            <table class="hrms-data-table compact w-100 t-2" id="grades-table">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($grades as $grade)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{$grade['name']}}</td>
 
+                        <td> 
+                            <button onclick="window.location='{{ route('admin.settings.grades.edit.post', ['id' => $grade->id]) }}';"
+                                    class="round-btn btn btn-default fas fa-edit btn-segment">
+                                </button>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+</div>
+@endsection
+ 
+@section('scripts')
+<script>
+    $('#grades-table').DataTable({
+            responsive: true,
+            stateSave: true,
+            dom: `<'row d-flex'<'col'l><'col d-flex justify-content-end'f><'col-auto d-flex justify-content-end'B>>" +
+            <'row'<'col-md-6'><'col-md-6'>>
+            <'row'<'col-md-12't>><'row'<'col-md-12'ip>>`,
+            buttons: [{
+                    extend: 'copy',
+                    text: '<i class="fas fa-copy "></i>',
+                    // text: '<i class="fas fa-copy "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Copy'
+                },
+                {
+                    extend: 'colvis',
+                    text: '<i class="fas fa-search "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Show/Hide Column'
+                },
+                {
+                    extend: 'csv',
+                    text: '<i class="fas fa-file-alt "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Export CSV'
+                },
+                {
+                    extend: 'print',
+                    text: '<i class="fas fa-print "></i>',
+                    className: 'btn-segment',
+                    titleAttr: 'Print'
+                },
+            ]
+    
+        });
+</script>
+@append
