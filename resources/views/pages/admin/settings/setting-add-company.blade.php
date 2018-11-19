146217c59fceb8492dafae1797e512090545bcde
SMSB\nsch <nsch@syntronic.com>
2018-11-19 14:47:54 +0800

    ADD: add new in admin/settings

---
+++ resources/views/pages/admin/settings/setting-add-company.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.app')
+@section('content')
+<div class="p-4">
+    <div class="card py-4 shadow-sm">
+        <div class="card-body">
+            <div class="container-fluid">
+                <form method="POST" action="{{ route('add_company') }}" id="form_validate"  data-parsley-validate>
+                    @csrf
+                    <div class="row">
+                        <div class="col-xl-8">
+                            <div class="form-group row">
+                                <label class="col-md-2 col-form-label">Code*</label>
+                                <div class="col-md-10">
+                                    <input id="code" type="text" class="form-control{{ $errors->has('code') ? ' is-invalid' : '' }}" name="code" value="{{ old('code') }}" required>                                     
+                                </div>
+                                <label class="col-md-2 col-form-label">Name*</label>
+                                <div class="col-md-10">
+                                    <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required>                                     
+                                </div>
+                                <label class="col-md-2 col-form-label">Full URL*</label>
+                                <div class="col-md-10">
+                                    <input id="url" type="text" class="form-control{{ $errors->has('url') ? ' is-invalid' : '' }}"
+                                        name="url" value="{{ old('url') }}" required>                                  
+                                </div>
+                                <label class="col-md-2 col-form-label">Registration Number*</label>
+                                <div class="col-md-10">
+                                    <input id="registration_number" type="text" class="form-control{{ $errors->has('registration_number') ? ' is-invalid' : '' }}"
+                                        name="registration_number" value="{{ old('registration_number') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">Description*</label>
+                                <div class="col-md-10">
+                                    <textarea id="description" type="text" class="form-control{{ $errors->has('description') ? ' is-invalid' : '' }}" name="description"
+                                        value="{{ old('description') }}" required>
+                                    </textarea> 
+                                </div>
+                                <label class="col-md-2 col-form-label">Address*</label>
+                                <div class="col-md-10">
+                                    <textarea id="address" type="Address" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address"
+                                        value="{{ old('address') }}" required>
+                                    </textarea>
+                                <label class="col-md-2 col-form-label">Phone No*</label>
+                                <div class="col-md-10">
+                                    <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}"
+                                        name="phone" value="{{ old('phone') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">TAX Number*</label>
+                                <div class="col-md-10">
+                                    <input id="tax_number" type="text" class="form-control{{ $errors->has('tax_number') ? ' is-invalid' : '' }}" 
+                                        name="tax_number" value="{{ old('tax_number') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">EPF Number*</label>
+                                <div class="col-md-10">
+                                    <input id="epf_number" type="text" class="form-control{{ $errors->has('epf_number') ? ' is-invalid' : '' }}"
+                                        name="epf_number" value="{{ old('epf_number') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">SOCSO Number*</label>
+                                <div class="col-md-10">
+                                    <input id="socso_number" type="text" class="form-control{{ $errors->has('socso_number') ? ' is-invalid' : '' }}"
+                                        name="socso_number" value="{{ old('socso_number') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">EIS Number*</label>
+                                <div class="col-md-10">
+                                    <input id="eis_number" type="text" class="form-control{{ $errors->has('eis_number') ? ' is-invalid' : '' }}"
+                                        name="eis_number" value="{{ old('eis_number') }}" required>
+                                </div>
+                                <label class="col-md-2 col-form-label">Status</label>
+                                <div class="col-md-10">
+                                    <select class="form-control" id="status" name="status">
+                                        <option value="Active">Active</option>
+                                        <option value="Inactive">Inactive</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row mb-0">
+                        <div class="col-md-6 offset-md-4">
+                            <button type="submit" class="btn btn-primary">
+                            {{ __('Submit') }}
+                        </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/pages/admin/settings/setting-grade.blade.php
@@ -0,0 +1,113 @@
+@extends('layouts.base') 
+@section('content')
+
+<!-- ADD -->
+<div class="modal fade" id="addGradePopup" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Add Grade</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('add_grade') }}" id="add_grade">
+                    @csrf
+                    <div class="row p-3">
+                        <div class="form-group row w-100">
+                            <label class="col-md-12 col-form-label">Grade name*</label>
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="Name here"
+                                    name="name" value="{{ old('name') }}" required>
+                            </div>                            
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">
+                        {{ __('Submit') }}
+                        </button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- UPDATE -->
+<div class="modal fade" id="updateGradePopup" tabindex="-1" role="dialog" aria-labelledby="updateGradeLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="updateGradeLabel">Edit Grade</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <form method="POST" action="{{ route('edit_grade') }}" id="edit_grade">
+                    @csrf
+                    <div class="row p-3">
+                        <div class="form-group row w-100">
+                            <input id="grade_id" name="grade_id" type="text" hidden>
+                            <label class="col-md-12 col-form-label">Grade Name*</label>
+                            <div class="col-md-12">
+                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="Name here"
+                                    name="name" value="{{ old('name') }}" required>
+                            </div>                                                        
+                        </div>
+                    </div>     
+                    <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary">
+                                {{ __('Submit') }}
+                            </button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>    
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="p-4">
+        <div class="card p-4">
+            <div class="card-body">
+                <div class="row pb-3">
+                    <div class="col-auto mr-auto"></div>
+                    <div class="col-auto">
+                        <button type="button" class="btn btn-outline-primary waves-effect" data-toggle="modal" data-target="#addGradePopup">
+                            Add Grade
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="float-right tableTools-container"></div>
+                        <table class="table display compact table-striped table-bordered table-hover w-100" id="setupJobconfigureCostCentreTable">
+                            <thead>
+                                    <tr>
+                                            <th>No</th>
+                                            <th>Name</th>
+                                            <th>Action</th>
+                                        </tr>
+                            </thead>
+                            <tbody>
+                                    @foreach($grade as $row)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{$row['name']}}</td>
+                                        <td><button class="btn btn-outline-primary waves-effect" data-toggle="modal"
+                                            data-grade-id="{{$row['id']}}"
+                                            data-grade-name="{{$row['name']}}"
+                                            data-target="#updateGradePopup">EDIT</button></td>
+                                    </tr>
+                                    </tr>
+                                    @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
+
