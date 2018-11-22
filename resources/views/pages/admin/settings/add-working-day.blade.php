857bb1788cc835b4142d39b9dd5c625381343d28
Adrian Miller <adrian.miller87@gmail.com>
2018-11-22 18:18:07 +0800

    NEW: Admin - Settings - Working Days - Add - Added error from backend if unable to add

--- resources/views/pages/admin/settings/add-working-day.blade.php
+++ resources/views/pages/admin/settings/add-working-day.blade.php
@@ -23,6 +23,11 @@
                             <div class="col-md-12">
                                 <input id="monday" type="number" min="0" max="1" step="0.5" value="1" class="form-control{{ $errors->has('monday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="monday" value="{{ old('monday') }}" required>
+                                    @if ($errors->has('monday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('monday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                        
@@ -31,6 +36,11 @@
                             <div class="col-md-12">
                                 <input id="tuesday" type="number" min="0" max="1" step="0.5" value="1" class="form-control{{ $errors->has('tuesday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="tuesday" value="{{ old('tuesday') }}" required>
+                                    @if ($errors->has('tuesday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('tuesday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                         <div class="form-group row w-100">
@@ -38,6 +48,11 @@
                             <div class="col-md-12">
                                 <input id="wednesday" type="number" min="0" max="1" step="0.5" value="1" class="form-control{{ $errors->has('wednesday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="wednesday" value="{{ old('wednesday') }}" required>
+                                    @if ($errors->has('wednesday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('wednesday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                         <div class="form-group row w-100">
@@ -45,6 +60,11 @@
                             <div class="col-md-12">
                                 <input id="thursday" type="number" min="0" max="1" step="0.5" value="1" class="form-control{{ $errors->has('thursday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="thursday" value="{{ old('thursday') }}" required>
+                                    @if ($errors->has('thursday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('thursday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                         <div class="form-group row w-100">
@@ -52,6 +72,11 @@
                             <div class="col-md-12">
                                 <input id="friday" type="number" min="0" max="1" step="0.5" value="1" class="form-control{{ $errors->has('friday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="friday" value="{{ old('friday') }}" required>
+                                    @if ($errors->has('friday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('friday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                         <div class="form-group row w-100">
@@ -59,6 +84,11 @@
                             <div class="col-md-12">
                                 <input id="saturday" type="number" min="0" max="1" step="0.5" value="0" class="form-control{{ $errors->has('saturday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="saturday" value="{{ old('saturday') }}" required>
+                                    @if ($errors->has('saturday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('saturday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                         <div class="form-group row w-100">
@@ -66,6 +96,11 @@
                             <div class="col-md-12">
                                 <input id="sunday" type="number" min="0" max="1" step="0.5" value="0" class="form-control{{ $errors->has('sunday') ? ' is-invalid' : '' }}" placeholder=""
                                     name="sunday" value="{{ old('sunday') }}" required>
+                                    @if ($errors->has('sunday'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('sunday') }}</strong>
+                                    </span> 
+                                    @endif
                             </div>                            
                         </div>
                     </div>
