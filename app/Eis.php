b23ad54fa000fe6a0676623ece7f630593f815c9
SMSB\nsch <nsch@syntronic.com>
2018-11-24 00:24:08 +0800

    FIx: to remove timestamps false

--- app/Eis.php
+++ app/Eis.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 class Eis extends Model
 {
     protected $table = 'eis';
-    public $timestamps = false;
+   
 
     protected $fillable = [
         'employer','employee','salary'
