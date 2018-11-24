81596835e49bb4af72b5b759574b7ee482aa0d22
Adrian Miller <adrian.miller87@gmail.com>
2018-11-24 10:34:52 +0800

    NEW: Added new Leave Types database implementation - migration, models, seeder(WIP)

---
+++ app/LeaveType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LeaveType extends Model
+{
+    use SoftDeletes;
+    protected $table = 'leave_types';
+
+    protected $fillable = [
+        'code',
+        'name',
+        'description',
+        'is_custom',
+        'entitled_days',
+        'active',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function applied_rules() {
+        return $this->hasMany('App\LTAppliedRule'); 
+    }
+
+    public function lt_conditional_entitlements() {
+        return $this->hasMany('App\LTConditionalEntitlement'); 
+    }
+}
