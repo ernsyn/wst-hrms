5ac27586b6ee9ad91a8f77389f212039f54b2915
Adrian Miller <asmi@syntronic.com>
2018-11-24 14:20:25 +0800

    NEW: Leave Allocations - Created migration and model

---
+++ app/LeaveAllocation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class LeaveAllocation extends Model
+{
+    use SoftDeletes;
+    protected $table = 'leave_allocations';
+
+    protected $fillable = [
+        'emp_id',
+        'leave_type_id',
+        'allocated_days',
+        'spent_days',
+        'carried_forward_days',
+        'is_carry_forward',
+        'valid_from_date',
+        'valid_until_date',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    public function leave_type() {
+        return $this->belongsTo('App\LeaveType'); 
+    }
+
+    protected $casts = [
+        'valid_from_date' => 'date:d-m-Y',
+        'valid_until_date' => 'date:d-m-Y',
+    ];
+}
