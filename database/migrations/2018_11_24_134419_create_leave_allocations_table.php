e0ba13f4c51ed660590d2e9e6efdb40038fe4cc0
Adrian Miller <asmi@syntronic.com>
2018-11-24 14:26:11 +0800

    NEW: Leave Allocations - Created migration and model (FIX: renamed and removed unnecessary files)

--- database/migrations/2018_11_24_134419_create_leave_allocations_table.php
+++ database/migrations/2018_11_24_134419_create_leave_allocations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class LeaveAllocationsTable extends Migration
+class CreateLeaveAllocationsTable extends Migration
 {
     /**
      * Run the migrations.
