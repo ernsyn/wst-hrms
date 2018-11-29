ece2994b5ee0fa67e2713a0a167c0f201e706c32
Adrian Miller <adrian.miller87@gmail.com>
2018-11-29 15:30:19 +0800

    NEW: Leave Service - Added function to get leave types and details for an employee

---
+++ app/Constants/LeaveTypeRule.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Constants;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeaveTypeRule
+{
+    const GENDER = 'gender';
+    const CAN_CARRY_FORWARD = 'can_carry_forward';
+    const MULTIPLE_APPROVAL_LEVELS_NEEDED = 'multiple_approval_levels_needed';
+    const REQUIRED_ATTACHMENT =  'required_attachment';
+    const MIN_APPLY_DAYS_BEFORE =  'min_apply_days_before';
+    const CONSECUTIVE =  'consecutive';
+    const SHARED_WITH =  'shared_with';
+    const MIN_EMPLOYMENT_PERIOD =  'min_employment_period';
+    const MAX_NO_OF_CHILDREN =  'max_no_of_children';
+    const UNPAID =  'unpaid';
+    const INC_OFF_DAYS_BASED_ON_APPLIED_DAYS =  'inc_off_days_based_on_applied_days';
+    const EMPLOYEE_CANNOT_APPLY =  'employee_cannot_apply';
+    const INC_OFF_DAYS =  'inc_off_days';
+    const MAX_AFTER_APPLIED_DAYS =  'max_after_applied_days';
+    const DEDUCT_AFTER_LEAVE_TYPES_INSUFFICIENT =  'deduct_after_leave_types_insufficient';
+    const MAX_APPLICATIONS =  'max_applications';
+    const NO_LIMIT =  'no_limit';
+    const MAX_DAYS_PER_APPLICATION =  'max_days_per_application';
+    // const LEAVE_CALCULATION =  'leave_calculation';
+}
+
+
--- app/Http/Services/LeaveService.php
+++ app/Http/Services/LeaveService.php
@@ -11,6 +11,9 @@ use App\LeaveAllocation;
 use App\LeaveRequest;
 use App\LeaveType;
 use App\EmployeeJob;
+use App\Employee;
+
+use App\Constants\LeaveTypeRule;
 
 class LeaveService
 {
@@ -95,10 +98,121 @@ class LeaveService
             }
         }
     }
+
+    public static function checkLeaveTypeValidity($emp_id, $leave_type_id, $start_date, $end_date) {
+        $leaveType = LeaveType::with('applied_rules')->where('id', $leave_type_id)->first();
+        
+        foreach($leaveType->applied_rules as $rule) {
+            switch ($rule->rule) {
+
+            }
+        }
+    }
+
+    public static function getLeaveTypesForUser(Employee $employee) {
+        $leaveTypes = LeaveType::with('applied_rules')->get();
+        $employee->gender;
+
+        Carbon::THURSDAY;
+        
+        $leaveTypesForUser = array();
+        foreach($leaveTypes as $leaveType) {
+            $additionalLeaveTypeDetails = array();
+            $includeLeaveType = true;
+            foreach($leaveType->applied_rules as $rule) {
+                switch($rule->rule) {
+                    case LeaveTypeRule::GENDER:
+                        $configuration = json_decode($rule->configuration);
+                        if(strcasecmp($employee->gender, $configuration->gender) != 0) {
+                            $includeLeaveType = false;
+                        }
+                        break  ;
+
+                    case LeaveTypeRule::CONSECUTIVE:
+                        // $configuration = json_decode($rule->configuration);
+                        $additionalLeaveTypeDetails['consecutive'] = true;
+                        break;
+                    case LeaveTypeRule::REQUIRED_ATTACHMENT:
+                        // $configuration = json_decode($rule->configuration);
+                        $additionalLeaveTypeDetails['required_attachment'] = true;
+                        break;
+                    case LeaveTypeRule::MAX_NO_OF_CHILDREN:
+                        $configuration = json_decode($rule->configuration);
+                        if($employee->total_children >= $configuration->max_no_of_children) {
+                            $includeLeaveType = false;
+                        }
+                        break;
+                    case LeaveTypeRule::EMPLOYEE_CANNOT_APPLY:
+                        $includeLeaveType = false;
+                        break;
+                    case LeaveTypeRule::MAX_APPLICATIONS:
+                        $configuration = json_decode($rule->configuration);
+                        if(strcasecmp($employee->gender, $configuration->gender) != 0) {
+                            $includeLeaveType = false;
+                        }
+                        break;
+                    case LeaveTypeRule::MIN_EMPLOYMENT_PERIOD:
+                        $configuration = json_decode($rule->configuration);
+
+                        $employedDays = self::calculateEmployeeWorkingDays($employee->id);
+                        if($employedDays < $configuration->min_days) {
+                            $includeLeaveType = false;
+                        }
+                        break;
+                    case LeaveTypeRule::NO_LIMIT:
+                        // $configuration = json_decode($rule->configuration);
+                        $additionalLeaveTypeDetails['no_limit'] = true;
+                        break;
+                    case LeaveTypeRule::MAX_DAYS_PER_APPLICATION:
+                        $configuration = json_decode($rule->configuration);
+                        $additionalLeaveTypeDetails['max_days_per_application'] = $configuration->max_days_per_application;
+                        break;   
+                }
+
+                if(!$includeLeaveType) {
+                    break;
+                }
+            }
+            
+            if($includeLeaveType) {
+                array_push($leaveTypesForUser, array_merge(
+                    [
+                        'name' => $leaveType->name,
+                        'description' => $leaveType->description,
+                        'available_days' => self::calculateLeaveTypeAvailableDaysForEmployee($employee->id, $leaveType->id)
+                    ], 
+                    $additionalLeaveTypeDetails)
+                );
+            }
+        }
+        
+        return $leaveTypesForUser;
+    }
     
+
+    // PRIVATE FUNCTIONS
+    private static function calculateLeaveTypeAvailableDaysForEmployee($emp_id, $leave_type_id) {
+        $today = Carbon::now();
+        $leaveAllocations = LeaveAllocation::where('emp_id', $emp_id)
+            ->where('leave_type_id', $leave_type_id)
+            ->where('valid_from_date', '<=', $today)
+            ->where('valid_until_date', '>=', $today)
+            // ->sum('allocated_days')
+            // ->sum('spent_days')
+            ->get();
+
+        $totalAllocatedDays = 0;
+        $totalSpentDays = 0;
+        foreach($leaveAllocations as $leaveAllocation) {
+            $totalAllocatedDays += $leaveAllocation->allocated_days;
+            $totalSpentDays += $leaveAllocation->spent_days;
+        }
+
+        return $totalAllocatedDays - $totalSpentDays;
+    }
+
     private static function leaveTypeHasRule($leaveType, $rule) {
         foreach($leaveType->applied_rules as $applied_rule) {
-            Log::debug($applied_rule->rule." ".$rule);
             if($applied_rule->rule == $rule) {
                 return true;
             }
@@ -106,6 +220,17 @@ class LeaveService
 
         return false;
     }
+
+    // private static function leaveTypeGetRule($leaveType, $rule) {
+    //     foreach($leaveType->applied_rules as $applied_rule) {
+    //         Log::debug($applied_rule->rule." ".$rule);
+    //         if($applied_rule->rule == $rule) {
+    //             return $applied_rule;
+    //         }
+    //     }
+
+    //     return null;
+    // }
 
     private static function calculateEntitledDays($leaveType, $yearsOfService, $grade_id) {
         $entitledDays = 0;
@@ -160,6 +285,19 @@ class LeaveService
         $nowDateTime = date_create();
         
         return date_diff($startDateTime, $nowDateTime)->y;
+    }
+
+    private static function calculateEmployeeWorkingDays($emp_id) {
+        // Get start_date of first job
+        $firstJob = EmployeeJob::where('emp_id', $emp_id)->orderBy('start_date')->first();
+        if(empty($firstJob)) {
+            return 0;
+        } 
+
+        $startDateTime = date_create($firstJob->start_date);
+        $nowDateTime = date_create();
+        
+        return date_diff($startDateTime, $nowDateTime)->days;
     }
 
 }
