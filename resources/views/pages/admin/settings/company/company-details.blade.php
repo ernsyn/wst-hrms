daa0b0b0a2c3eb7242028bee8a1e2dc9370b34a7
SMSB\nsch <nsch@syntronic.com>
2018-12-02 04:43:18 +0800

    fix: addition and deduction , few error

--- resources/views/pages/admin/settings/company/company-details.blade.php
+++ resources/views/pages/admin/settings/company/company-details.blade.php
@@ -641,7 +641,7 @@
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Security Group</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Edit Company Addition</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
