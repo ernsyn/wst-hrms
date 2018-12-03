22c37f0074bceb945bfdb28002b7180f4d3cc6d2
chennee <chennee.lim@wisetech.com.my>
2018-12-03 08:45:30 +0800

    FIX: add payroll

--- app/Repositories/Payroll/EloquentAddition.php
+++ app/Repositories/Payroll/EloquentAddition.php
@@ -15,23 +15,23 @@ class EloquentAddition implements AdditionRepository
         Addition::select('additions.*')
         ->leftjoin('addition_applies_to as AAT', 'AAT.additions_id', '=', 'additions.id')
         ->where(function($query) use($companyId){
-            $query->where('Additions.company_id', $companyId)
-            ->orwhere('Additions.company_id', 0);
+            $query->where('additions.company_id', $companyId)
+            ->orwhere('additions.company_id', 0);
         })
         ->where(function($query) use($cost_center_id, $grade_id){
             if($cost_center_id && $grade_id) {
                 $query->whereIn('AAT.job_master_id', [$cost_center_id, $grade_id])
-                ->orwhere('Additions.company_id', 0);
+                ->orwhere('additions.company_id', 0);
             }
         })
         ->where(function($query) use($status){
             if($status) {
                 if($status == 'Probationer') {
-                    $query->where('Additions.confirmed_employee', 0);
+                    $query->where('additions.confirmed_employee', 0);
                 } else {
-                    $query->where('Additions.confirmed_employee', 1);
+                    $query->where('additions.confirmed_employee', 1);
                 }
-                $query->orwhere('Additions.company_id', 0);
+                $query->orwhere('additions.company_id', 0);
             }
         })
 //         ->groupby('AdditionMaster.id')
--- app/Repositories/Payroll/EloquentDeduction.php
+++ app/Repositories/Payroll/EloquentDeduction.php
@@ -13,26 +13,26 @@ class EloquentDeduction implements DeductionRepository
         $companyId = $filter['companyId'];
         
         return
-        Deduction::select('Deductions.*')
+        Deduction::select('deductions.*')
         ->leftjoin('deduction_applies_to as DAT', 'DAT.deductions_id', '=', 'Deductions.id')
         ->where(function($query) use($companyId){
-            $query->where('Deductions.company_id', $companyId)
-            ->orwhere('Deductions.company_id', 0);
+            $query->where('deductions.company_id', $companyId)
+            ->orwhere('deductions.company_id', 0);
         })
         ->where(function($query) use($cost_center_id, $grade_id){
             if($cost_center_id && $grade_id) {
                 $query->whereIn('DAT.job_master_id', [$cost_center_id, $grade_id])
-                ->orwhere('Deductions.company_id', 0);
+                ->orwhere('deductions.company_id', 0);
             }
         })
         ->where(function($query) use($status){
             if($status) {
                 if($status == 'Probationer') {
-                    $query->where('Deductions.confirmed_employee', 0);
+                    $query->where('deductions.confirmed_employee', 0);
                 } else {
-                    $query->where('Deductions.confirmed_employee', 1);
+                    $query->where('deductions.confirmed_employee', 1);
                 }
-                $query->orwhere('Deductions.company_id', 0);
+                $query->orwhere('deductions.company_id', 0);
             }
         })
 //         ->groupby('DeductionMaster.id')
