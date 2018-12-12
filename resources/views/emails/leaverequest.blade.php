2ce44e84704023a78604c3089042b55b1a896d74
SMSB\sals <sals@syntronic.com>
2018-12-12 14:02:16 +0800

    Updated email template

--- resources/views/emails/leaverequest.blade.php
+++ resources/views/emails/leaverequest.blade.php
@@ -14,7 +14,7 @@
 
 	<p>Hi Admin,</p>
 
-	<p><strong>{{ $user->name }}</strong> has applied for <strong>{{ $leaveRequest->applied_days }}</strong> day(s) of leave, from <strong>{{ \Carbon\Carbon::parse($leaveRequest->start_date)->format('d/m/Y') }}</strong> to <strong>{{ \Carbon\Carbon::parse($leaveRequest->end_date)->format('d/m/Y') }}</strong>.</p>
+	<p><strong>{{ $user->name }}</strong> has applied for <strong>{{ $leaveRequest->applied_days }} {{ $leaveRequest->am_pm }}</strong> day(s) of leave, from <strong>{{ \Carbon\Carbon::parse($leaveRequest->start_date)->format('d/m/Y') }}</strong> to <strong>{{ \Carbon\Carbon::parse($leaveRequest->end_date)->format('d/m/Y') }}</strong>.</p>
 
 	<p>The reason for the leave request is:</p>
 
