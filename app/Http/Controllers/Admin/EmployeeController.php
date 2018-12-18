d055221a170cb20f38776f5e061274c8d6ed2995
SMSB\sals <sals@syntronic.com>
2018-12-18 10:16:52 +0800

    Updated Employee Working Days to ENUM

--- app/Http/Controllers/Admin/EmployeeController.php
+++ app/Http/Controllers/Admin/EmployeeController.php
@@ -683,13 +683,13 @@ $jobData['status']  = 'probationer';
     public function postWorkingDay(Request $request, $id)
     {
         $workingDayData = $request->validate([
-            'monday' => 'required|in:0,0.5,1',
-            'tuesday' => 'required|in:0,0.5,1',
-            'wednesday' => 'required|in:0,0.5,1',
-            'thursday' => 'required|in:0,0.5,1',
-            'friday' => 'required|in:0,0.5,1',
-            'saturday' => 'required|in:0,0.5,1',
-            'sunday' => 'required|in:0,0.5,1',
+            'monday' => 'required|in:full,half,off,rest',
+            'tuesday' => 'required|in:full,half,off,rest',
+            'wednesday' => 'required|in:full,half,off,rest',
+            'thursday' => 'required|in:full,half,off,rest',
+            'friday' => 'required|in:full,half,off,rest',
+            'saturday' => 'required|in:full,half,off,rest',
+            'sunday' => 'required|in:full,half,off,rest',
             'start_work_time' => 'required',
             'end_work_time' => 'required',
         ]);
@@ -707,13 +707,13 @@ $jobData['status']  = 'probationer';
     public function postEditWorkingDay(Request $request, $id)
     {
         $workingDayUpdateData = $request->validate([
-            'monday' => 'required|in:0,0.5,1',
-            'tuesday' => 'required|in:0,0.5,1',
-            'wednesday' => 'required|in:0,0.5,1',
-            'thursday' => 'required|in:0,0.5,1',
-            'friday' => 'required|in:0,0.5,1',
-            'saturday' => 'required|in:0,0.5,1',
-            'sunday' => 'required|in:0,0.5,1',
+            'monday' => 'required|in:full,half,off,rest',
+            'tuesday' => 'required|in:full,half,off,rest',
+            'wednesday' => 'required|in:full,half,off,rest',
+            'thursday' => 'required|in:full,half,off,rest',
+            'friday' => 'required|in:full,half,off,rest',
+            'saturday' => 'required|in:full,half,off,rest',
+            'sunday' => 'required|in:full,half,off,rest',
             'start_work_time' => 'required',
             'end_work_time' => 'required',
         ]);
