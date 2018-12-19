4de309bbe5e54f19181e8829e610cbd109a69652
chennee <chennee-wst@wisetech.com.my>
2018-12-19 17:11:05 +0800

    NEW: EPF, EIS, SOCSO Contribution table

--- database/seeds/EisTableSeeder.php
+++ database/seeds/EisTableSeeder.php
@@ -48,5 +48,232 @@ class EisTableSeeder extends Seeder
             'employee' => 0.35
         ]);
         
+        Eis::create([
+            'salary' => 300.00,
+            'employer' => 0.50,
+            'employee' => 0.50
+        ]);
+        
+        Eis::create([
+            'salary' => 400.00,
+            'employer' => 0.70,
+            'employee' => 0.70
+        ]);
+        
+        Eis::create([
+            'salary' => 500.00,
+            'employer' => 0.90,
+            'employee' => 0.90
+        ]);
+        
+        Eis::create([
+            'salary' => 600.00,
+            'employer' => 1.10,
+            'employee' => 1.10
+        ]);
+        
+        Eis::create([
+            'salary' => 700.00,
+            'employer' => 1.30,
+            'employee' => 1.30
+        ]);
+        
+        Eis::create([
+            'salary' => 800.00,
+            'employer' => 1.50,
+            'employee' => 1.50
+        ]);
+        
+        Eis::create([
+            'salary' => 900.00,
+            'employer' => 1.70,
+            'employee' => 1.70
+        ]);
+        
+        Eis::create([
+            'salary' => 1000.00,
+            'employer' => 1.90,
+            'employee' => 1.90
+        ]);
+        
+        Eis::create([
+            'salary' => 1100.00,
+            'employer' => 2.10,
+            'employee' => 2.10
+        ]);
+        
+        Eis::create([
+            'salary' => 1200.00,
+            'employer' => 2.30,
+            'employee' => 2.30
+        ]);
+        
+        Eis::create([
+            'salary' => 1300.00,
+            'employer' => 2.50,
+            'employee' => 2.50
+        ]);
+        
+        Eis::create([
+            'salary' => 1400.00,
+            'employer' => 2.70,
+            'employee' => 2.70
+        ]);
+        
+        Eis::create([
+            'salary' => 1500.00,
+            'employer' => 2.90,
+            'employee' => 2.90
+        ]);
+        
+        Eis::create([
+            'salary' => 1600.00,
+            'employer' => 3.10,
+            'employee' => 3.10
+        ]);
+        
+        Eis::create([
+            'salary' => 1700.00,
+            'employer' => 3.30,
+            'employee' => 3.30
+        ]);
+        
+        Eis::create([
+            'salary' => 1800.00,
+            'employer' => 3.50,
+            'employee' => 3.50
+        ]);
+        
+        Eis::create([
+            'salary' => 1900.00,
+            'employer' => 3.70,
+            'employee' => 3.70
+        ]);
+        
+        Eis::create([
+            'salary' => 2000.00,
+            'employer' => 3.90,
+            'employee' => 3.90
+        ]);
+        
+        Eis::create([
+            'salary' => 2100.00,
+            'employer' => 4.10,
+            'employee' => 4.10
+        ]);
+        
+        Eis::create([
+            'salary' => 2200.00,
+            'employer' => 4.30,
+            'employee' => 4.30
+        ]);
+        
+        Eis::create([
+            'salary' => 2300.00,
+            'employer' => 4.50,
+            'employee' => 4.50
+        ]);
+        
+        Eis::create([
+            'salary' => 2400.00,
+            'employer' => 4.70,
+            'employee' => 4.70
+        ]);
+        
+        Eis::create([
+            'salary' => 2500.00,
+            'employer' => 4.90,
+            'employee' => 4.90
+        ]);
+        
+        Eis::create([
+            'salary' => 2600.00,
+            'employer' => 5.10,
+            'employee' => 5.10
+        ]);
+        
+        Eis::create([
+            'salary' => 2700.00,
+            'employer' => 5.30,
+            'employee' => 5.30
+        ]);
+        
+        Eis::create([
+            'salary' => 2800.00,
+            'employer' => 5.50,
+            'employee' => 5.50
+        ]);
+        
+        Eis::create([
+            'salary' => 2900.00,
+            'employer' => 5.70,
+            'employee' => 5.70
+        ]);
+        
+        Eis::create([
+            'salary' => 3000.00,
+            'employer' => 5.90,
+            'employee' => 5.90
+        ]);
+        
+        Eis::create([
+            'salary' => 3100.00,
+            'employer' => 6.10,
+            'employee' => 6.10
+        ]);
+        
+        Eis::create([
+            'salary' => 3200.00,
+            'employer' => 6.30,
+            'employee' => 6.30
+        ]);
+        
+        Eis::create([
+            'salary' => 3300.00,
+            'employer' => 6.50,
+            'employee' => 6.50
+        ]);
+        
+        Eis::create([
+            'salary' => 3400.00,
+            'employer' => 6.70,
+            'employee' => 6.70
+        ]);
+        
+        Eis::create([
+            'salary' => 3500.00,
+            'employer' => 6.90,
+            'employee' => 6.90
+        ]);
+        
+        Eis::create([
+            'salary' => 3600.00,
+            'employer' => 7.10,
+            'employee' => 7.10
+        ]);
+        
+        Eis::create([
+            'salary' => 3700.00,
+            'employer' => 7.30,
+            'employee' => 7.30
+        ]);
+        
+        Eis::create([
+            'salary' => 3800.00,
+            'employer' => 7.50,
+            'employee' => 7.50
+        ]);
+        
+        Eis::create([
+            'salary' => 3900.00,
+            'employer' => 7.70,
+            'employee' => 7.70
+        ]);
+        
+        Eis::create([
+            'salary' => 4000.00,
+            'employer' => 7.90,
+            'employee' => 7.90
+        ]);
     }
 }
--- database/seeds/SocsoTableSeeder.php
+++ database/seeds/SocsoTableSeeder.php
@@ -47,5 +47,233 @@ class SocsoTableSeeder extends Seeder
             'first_category_employer' => 2.95,
             'first_category_employee' => 0.85
         ]);
+        
+        Socso::create([
+            'salary' => 300.00,
+            'first_category_employer' => 4.35,
+            'first_category_employee' => 1.25
+        ]);
+        
+        Socso::create([
+            'salary' => 400.00,
+            'first_category_employer' => 6.15,
+            'first_category_employee' => 1.75
+        ]);
+        
+        Socso::create([
+            'salary' => 500.00,
+            'first_category_employer' => 7.85,
+            'first_category_employee' => 2.25
+        ]);
+        
+        Socso::create([
+            'salary' => 600.00,
+            'first_category_employer' => 9.65,
+            'first_category_employee' => 2.75
+        ]);
+        
+        Socso::create([
+            'salary' => 700.00,
+            'first_category_employer' => 11.35,
+            'first_category_employee' => 3.25
+        ]);
+        
+        Socso::create([
+            'salary' => 800.00,
+            'first_category_employer' => 13.15,
+            'first_category_employee' => 3.75
+        ]);
+        
+        Socso::create([
+            'salary' => 900.00,
+            'first_category_employer' => 14.85,
+            'first_category_employee' => 4.25
+        ]);
+        
+        Socso::create([
+            'salary' => 1000.00,
+            'first_category_employer' => 16.65,
+            'first_category_employee' => 4.75
+        ]);
+        
+        Socso::create([
+            'salary' => 1100.00,
+            'first_category_employer' => 18.35,
+            'first_category_employee' => 5.25
+        ]);
+        
+        Socso::create([
+            'salary' => 1200.00,
+            'first_category_employer' => 20.15,
+            'first_category_employee' => 5.75
+        ]);
+        
+        Socso::create([
+            'salary' => 1300.00,
+            'first_category_employer' => 21.85,
+            'first_category_employee' => 6.25
+        ]);
+        
+        Socso::create([
+            'salary' => 1400.00,
+            'first_category_employer' => 23.65,
+            'first_category_employee' => 6.75
+        ]);
+        
+        Socso::create([
+            'salary' => 1500.00,
+            'first_category_employer' => 25.35,
+            'first_category_employee' => 7.25
+        ]);
+        
+        Socso::create([
+            'salary' => 1600.00,
+            'first_category_employer' => 27.15,
+            'first_category_employee' => 7.75
+        ]);
+        
+        Socso::create([
+            'salary' => 1700.00,
+            'first_category_employer' => 28.85,
+            'first_category_employee' => 8.25
+        ]);
+        
+        Socso::create([
+            'salary' => 1800.00,
+            'first_category_employer' => 30.65,
+            'first_category_employee' => 8.75
+        ]);
+        
+        Socso::create([
+            'salary' => 1900.00,
+            'first_category_employer' => 32.35,
+            'first_category_employee' => 9.25
+        ]);
+        
+        Socso::create([
+            'salary' => 2000.00,
+            'first_category_employer' => 34.15,
+            'first_category_employee' => 9.75
+        ]);
+        
+        Socso::create([
+            'salary' => 2100.00,
+            'first_category_employer' => 35.85,
+            'first_category_employee' => 10.25
+        ]);
+        
+        Socso::create([
+            'salary' => 2200.00,
+            'first_category_employer' => 37.65,
+            'first_category_employee' => 10.75
+        ]);
+        
+        Socso::create([
+            'salary' => 2300.00,
+            'first_category_employer' => 39.35,
+            'first_category_employee' => 11.25
+        ]);
+        
+        Socso::create([
+            'salary' => 2400.00,
+            'first_category_employer' => 41.15,
+            'first_category_employee' => 11.75
+        ]);
+        
+        Socso::create([
+            'salary' => 2500.00,
+            'first_category_employer' => 42.85,
+            'first_category_employee' => 12.25
+        ]);
+        
+        Socso::create([
+            'salary' => 2600.00,
+            'first_category_employer' => 44.65,
+            'first_category_employee' => 12.75
+        ]);
+        
+        Socso::create([
+            'salary' => 2700.00,
+            'first_category_employer' => 46.35,
+            'first_category_employee' => 12.25
+        ]);
+        
+        Socso::create([
+            'salary' => 2800.00,
+            'first_category_employer' => 48.15,
+            'first_category_employee' => 13.75
+        ]);
+        
+        Socso::create([
+            'salary' => 2900.00,
+            'first_category_employer' => 49.85,
+            'first_category_employee' => 14.25
+        ]);
+        
+        Socso::create([
+            'salary' => 3000.00,
+            'first_category_employer' => 51.65,
+            'first_category_employee' => 14.75
+        ]);
+        
+        Socso::create([
+            'salary' => 3100.00,
+            'first_category_employer' => 53.35,
+            'first_category_employee' => 15.25
+        ]);
+        
+        Socso::create([
+            'salary' => 3200.00,
+            'first_category_employer' => 55.15,
+            'first_category_employee' => 15.75
+        ]);
+        
+        Socso::create([
+            'salary' => 3300.00,
+            'first_category_employer' => 56.85,
+            'first_category_employee' => 16.25
+        ]);
+        
+        Socso::create([
+            'salary' => 3400.00,
+            'first_category_employer' => 58.65,
+            'first_category_employee' => 16.75
+        ]);
+        
+        Socso::create([
+            'salary' => 3500.00,
+            'first_category_employer' => 60.35,
+            'first_category_employee' => 17.25
+        ]);
+        
+        Socso::create([
+            'salary' => 3600.00,
+            'first_category_employer' => 62.15,
+            'first_category_employee' => 17.75
+        ]);
+        
+        Socso::create([
+            'salary' => 3700.00,
+            'first_category_employer' => 63.85,
+            'first_category_employee' => 18.25
+        ]);
+        
+        Socso::create([
+            'salary' => 3800.00,
+            'first_category_employer' => 65.65,
+            'first_category_employee' => 18.75
+        ]);
+        
+        Socso::create([
+            'salary' => 3900.00,
+            'first_category_employer' => 67.35,
+            'first_category_employee' => 19.25
+        ]);
+        
+        Socso::create([
+            'salary' => 4000.00,
+            'first_category_employer' => 69.05,
+            'first_category_employee' =>19.75
+        ]);
     }
 }
