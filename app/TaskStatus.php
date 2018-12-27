534c20826e0992aadd0cfd58d584293f14059a71
Adrian Miller <asmi@syntronic.com>
2018-12-27 17:59:23 +0800

    NEW: Task for generating attendances per day NEW: CRON for running attendance generation daily

---
+++ app/TaskStatus.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class TaskStatus extends Model
+{
+    protected $table = 'task_status';
+
+    protected $fillable =[
+        'task' ,
+        'status'
+    ];
+}
