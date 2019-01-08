3ba4a8919e1efd8984fde9061bf96bd1c5eb856f
SMSB\mmkb <kamilkahar90@gmail.com>
2019-01-08 17:34:21 +0800

    FIX: add dependent button ui

--- resources/views/pages/employee/id/dependents.blade.php
+++ resources/views/pages/employee/id/dependents.blade.php
@@ -131,7 +131,7 @@
     <div class="row pb-3">
         <div class="col-auto mr-auto"></div>
         <div class="col-auto">
-            <button type="button" class="btn btn-outline-info waves-effect" data-toggle="modal" data-target="#add-dependent-popup">
+            <button type="button" class="btn btn-primary waves-effect" data-toggle="modal" data-target="#add-dependent-popup">
                 Add Dependent
             </button>
         </div>
