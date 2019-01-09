3c2f6c85f9c32d3e62fc0e73161b962eb43ce7f6
SMSB\sals <sals@syntronic.com>
2019-01-09 16:30:11 +0800

    Fixed attendance report clock_out data

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -26,7 +26,7 @@ class AttendanceController extends Controller
         ->join('employees', 'users.id', '=', 'employees.user_id')
         ->join('employee_clock_in_out_records', 'employees.id', '=', 'employee_clock_in_out_records.emp_id')
         ->select('users.name','employees.code','employee_clock_in_out_records.*')
-        ->whereDate('employee_clock_in_out_records.clock_in_time', \Carbon\Carbon::today())
+        ->whereDate('employee_clock_in_out_records.clock_in_time', Carbon::today())
         ->get();
 
         $result_array = array();
@@ -34,13 +34,13 @@ class AttendanceController extends Controller
         foreach ($attendances as $row) {
             $result_array[$row->id]['code'] = $row->code;
             $result_array[$row->id]['name'] = $row->name;
-            $result_array[$row->id]['clock_in_time'] = $row->clock_in_time;
+            $result_array[$row->id]['clock_in_time'] = Carbon::parse($row->clock_in_time)->format('g:m:s a');
             $result_array[$row->id]['clock_in_lat'] = $row->clock_in_lat;
             $result_array[$row->id]['clock_in_long'] = $row->clock_in_long;
             $result_array[$row->id]['clock_in_address'] = $row->clock_in_address;
             $result_array[$row->id]['clock_in_status'] = $row->clock_in_status;
             $result_array[$row->id]['clock_in_reason'] = $row->clock_in_reason;
-            $result_array[$row->id]['clock_out_time'] = $row->clock_out_time;
+            $result_array[$row->id]['clock_out_time'] = $row->clock_out_time ? Carbon::parse($row->clock_out_time)->format('g:m:s a') : '';
             $result_array[$row->id]['clock_out_lat'] = $row->clock_out_lat;
             $result_array[$row->id]['clock_out_long'] = $row->clock_out_long;
             $result_array[$row->id]['clock_out_address'] = $row->clock_out_address;
@@ -50,16 +50,25 @@ class AttendanceController extends Controller
             $media_in = Media::where('id', $row->clock_in_image_media_id)->first();
             $media_out = Media::where('id', $row->clock_out_image_media_id)->first();
 
+            $media_in_img = '';
             $media_out_img = '';
+            $media_na_img = 'http://placehold.jp/24/cccccc/ffffff/300x300.png?text=NA';
+
+            if($media_in) {
+                $media_in_img = 'data:'.$media_in->mimetype.';base64,'.$media_in->data;
+            }
+            else {
+                $media_in_img = $media_na_img;
+            }
 
             if($media_out) {
                 $media_out_img = 'data:'.$media_out->mimetype.';base64,'.$media_out->data;
             }
             else {
-                $media_out_image = 'http://placehold.jp/24/cccccc/ffffff/300x300.png?text=NA';
+                $media_out_img = $media_na_img;
             }
 
-            $result_array[$row->id]['clock_in_media'] = 'data:'.$media_in->mimetype.';base64,'.$media_in->data;
+            $result_array[$row->id]['clock_in_media'] = $media_in_img;
             $result_array[$row->id]['clock_out_media'] = $media_out_img;
         }
 
