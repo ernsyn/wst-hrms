a53ac7c95ae9664ab4b8d785eb174744db880a2d
Lim Chen Nee <chennee.lim@wisetech.com.my>
2019-01-15 11:44:17 +0800

    NEW: Laravel Auditing

--- app/PayrollMaster.php
+++ app/PayrollMaster.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class PayrollMaster extends Model
+class PayrollMaster extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     protected $table = 'payroll_master';
 
     /*
--- app/PayrollTrx.php
+++ app/PayrollTrx.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
 
-class PayrollTrx extends Model
+class PayrollTrx extends Model implements Auditable
 {
+    use \OwenIt\Auditing\Auditable;
+    
     protected $table = 'payroll_trx';
     
     protected $fillable = [
