f45d97e97f8bd3012a33689c00f36a322d9ff466
Ng Han Shen <nghs@syntronic.com>
2019-02-08 16:30:42 +0800

    FIX: Crashes at superadmin mode (superadmin don't have employee information)

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -10,7 +10,12 @@
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
                         aria-haspopup="true" aria-expanded="false" v-pre>
                         {{-- <i class="default-user-logo-light fas fa-user-circle fa-2x px-3 align-middle"></i> --}}
-                        @if (Auth::user()->employee->profile_media != null )
+
+                        @if (Auth::user()->name == 'Super Admin')
+                        
+                        <i class="default-user-logo-light fas fa-user-circle fa-2x px-3 align-middle"></i>
+
+                        @elseif (Auth::user()->employee->profile_media != null )
                             <img class="rounded-circle mx-3" src="data:{{Auth::user()->employee->profile_media->mimetype}};base64, {{Auth::user()->employee->profile_media->data}}"  style="object-fit:cover; width:28px; height:28px">
                         @else
                             <i class="default-user-logo-light fas fa-user-circle fa-2x px-3 align-middle"></i>
