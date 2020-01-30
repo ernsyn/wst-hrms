8db2cd907e73b23db40552edd4f4e457b5e72895
Khang Jieh <khangjieh@gmail.com>
2020-01-30 12:45:42 +0800

    FIX: #94 redo resignation & termination

---
+++ database/migrations/2020_01_29_142725_add_blacklisted_and_reason_to_employee_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBlacklistedAndReasonToEmployeeJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employee_jobs', function (Blueprint $table) {
+        	$table->datetime('resignation_date')->nullable();
+            $table->boolean('blacklisted')->default('0');
+            $table->string('reason',200)->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employee_jobs', function (Blueprint $table) {
+        	$table->dropColumn('resignation_date');
+            $table->dropColumn('blacklisted');
+            $table->dropColumn('reason');
+        });
+    }
+}
